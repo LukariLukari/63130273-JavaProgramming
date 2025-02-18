dc01a377582475f09637cae69e23fc3239be8ad3
Lukari <lukari87@gmail.com>
2025-02-18 13:55:53 +0700

    tạo
    tạo array và ví dụ nhập xuất

---
+++ BaitapJava/ArrayList/src/CT.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class CT {
+public static void main (String[] args) {
+// Khai báo danh sách sản phẩm
+ArrayList<SanPham> dsSanPham;
+//Xin mới
+dsSanPham = new ArrayList<SanPham> () ;
+
+// Nhập 3 sản phẩm
+SanPham spl - new SanPham(0, null, null, null);
+SanPham sp2 - new SanPham(1, null, null, null);
+SanPham sp3 = new SanPham(2, null, null, null);
+
+//Thêm vào Arrylist
+dsSanPham.add (spl) ;
+dsSanPham. add (sp2) ;
+dsSanPham. add (sp3) ;
+}
---
+++ BaitapJava/ViDuNhapXuat1/src/ntu/CTTinhTong.java
@@ -0,0 +1,5 @@
+package ntu;
+
+public class CTTinhTong {
+
+}
