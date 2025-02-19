d2d5fb20b048bde2a6fc1493ec4581618129d982
Lukari <lukari87@gmail.com>
2025-02-19 20:37:28 +0700

    Update CT.java

--- BaitapJava/ArrayList/src/CT.java
+++ BaitapJava/ArrayList/src/CT.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
 
 public class CT {
-
+	public static void  main (String [] args) {
+		//Khai báo danh sách sản phẩm
+		ArrayList<SanPham> dsSanPham;
+		
+		//Xin moi
+		SanPham sp1 = new SanPham(0, "Kem", "Giải khát", null);
+		SanPham sp2 = new SanPham(1, "Cơm", "Đồ ăn", null);
+		SanPham sp3 = new SanPham(2, "Phở", "Đồ ăn", null);
+		
+		//Them vao Arraylist
+		dsSanPham.add(sp1);
+		dsSanPham.add(sp2);
+		dsSanPham.add(sp3);
+		
+		for 
+	}
 }
