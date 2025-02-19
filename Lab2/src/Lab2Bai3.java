a0f047ce4b3f89513bb2cfa6af0c8bc068f52e8e
Lukari <lukari87@gmail.com>
2025-02-19 21:13:06 +0700

    bai3lab2

---
+++ Lab2/src/Lab2Bai3.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+import java.util.*;
+public class Lab2Bai3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập vào số tiền điện sử dụng của tháng:");
+		double sodien = sc.nextDouble();
+		if(sodien<=50) {
+			System.out.printf("Tiền điện là: %.1f",sodien*1000);
+		}
+		else {
+		
+	}
+
+}
