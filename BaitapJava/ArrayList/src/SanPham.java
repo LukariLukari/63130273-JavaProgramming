12baf05095c3b50c461a6d1545c1a87805002e3f
Lukari <lukari87@gmail.com>
2025-02-19 20:30:53 +0700

    Hướng đối tượng

--- BaitapJava/ArrayList/src/SanPham.java
+++ BaitapJava/ArrayList/src/SanPham.java
@@ -1,4 +1,46 @@
 
 public class SanPham {
-
+// Định nghĩa các thuộc tính
+int maSP;
+String tenSP;
+String loaisP;
+String anhSP;
+public SanPham() {
+	super();
 }
+public SanPham(int maSP, String tenSP, String loaisP, String anhSP) {
+	super();
+	this.maSP = maSP;
+	this.tenSP = tenSP;
+	this.loaisP = loaisP;
+	this.anhSP = anhSP;
+}
+public int getMaSP() {
+	return maSP;
+}
+public void setMaSP(int maSP) {
+	this.maSP = maSP;
+}
+public String getTenSP() {
+	return tenSP;
+}
+public void setTenSP(String tenSP) {
+	this.tenSP = tenSP;
+}
+public String getLoaisP() {
+	return loaisP;
+}
+public void setLoaisP(String loaisP) {
+	this.loaisP = loaisP;
+}
+public String getAnhSP() {
+	return anhSP;
+}
+public void setAnhSP(String anhSP) {
+	this.anhSP = anhSP;
+}
+@Override
+public String toString() {
+	return "[Mã SP=" + maSP + ", Tên SP=" + tenSP + ", Loại SP=" + loaisP + ", Ảnh SP=" + anhSP + "]";
+} //tên file ảnh
+
